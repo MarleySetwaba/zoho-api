46e1b08d4ca88f08a93418f46a8554b67be357f8
MarleySetwaba <73256020+MarleySetwaba@users.noreply.github.com>
2024-10-09 15:47:00 +0200

    Update zoho_types.ts

--- Zoho_Inventory/zoho_types.ts
+++ Zoho_Inventory/zoho_types.ts
@@ -1,64 +1,274 @@
-const items = {
+
+const dummy_items = {
     "code": 0,
     "message": "success",
     "items": [
         {
-            "group_id": 4815000000044220,
-            "group_name": "Bags",
-            "item_id": 4815000000044208,
-            "name": "Bags-small",
+            "item_id": "3850050000000128011",
+            "name": "CASTROL ENGINE OIL GTX 5L",
+            "item_name": "CASTROL ENGINE OIL GTX 5L",
+            "unit": "ml",
             "status": "active",
-            "source": "string",
-            "is_linked_with_zohocrm": false,
-            "item_type": "inventory",
-            "description": "description",
-            "rate": 6,
-            "is_taxable": true,
-            "tax_id": 4815000000044043,
-            "tax_name": "Sales",
-            "tax_percentage": 12,
-            "purchase_description": "Purchase description",
-            "purchase_rate": 6,
+            "source": "user",
             "is_combo_product": false,
+            "is_linked_with_zohocrm": false,
+            "zcrm_product_id": "",
+            "description": "",
+            "brand": "",
+            "manufacturer": "",
+            "rate": 360.00,
+            "tax_id": "3850050000000076117",
+            "tax_name": "VAT",
+            "tax_percentage": 15,
+            "purchase_account_id": "3850050000000034003",
+            "purchase_account_name": "Cost of Goods Sold",
+            "account_id": "3850050000000000388",
+            "account_name": "Sales",
+            "purchase_description": "",
+            "purchase_rate": 200.00,
+            "item_type": "inventory",
             "product_type": "goods",
-            "attribute_id1": 4815000000044112,
-            "attribute_name1": "Small",
-            "reorder_level": 5,
-            "stock_on_hand": 50,
-            "available_stock": 2,
-            "actual_available_stock": 2,
-            "sku": "SK123",
-            "upc": 111111111111,
-            "ean": 111111111112,
-            "isbn": 111111111113,
-            "part_number": 111111111114,
-            "attribute_option_id1": 4815000000044214,
-            "attribute_option_name1": "Small",
-            "image_name": "bag_s.jpg",
-            "image_type": "jpg",
-            "created_time": "2013-01-24",
-            "last_modified_time": "2013-01-24",
-            "hsn_or_sac": 85423100,
-            "sat_item_key_code": "string",
-            "unitkey_code": "string",
-            "custom_fields": [
-                {
-                    "customfield_id": "46000000012845",
-                    "value": "Normal"
-                }
-            ]
+            "stock_on_hand": 5.000000,
+            "has_attachment": false,
+            "is_returnable": false,
+            "available_stock": 5.000000,
+            "actual_available_stock": 5.000000,
+            "sku": "GTX5L",
+            "upc": "",
+            "ean": "",
+            "isbn": "",
+            "part_number": "",
+            "reorder_level": "",
+            "image_name": "",
+            "image_type": "",
+            "image_document_id": "",
+            "created_time": "2024-02-01T10:54:54+0200",
+            "last_modified_time": "2024-02-01T10:55:14+0200",
+            "length": "",
+            "width": "",
+            "height": "",
+            "weight": "",
+            "weight_unit": "kg",
+            "dimension_unit": "cm"
         },
- 
+        {
+            "item_id": "3850050000000131003",
+            "name": "DISC PADS D345",
+            "item_name": "DISC PADS D345",
+            "unit": "D345",
+            "status": "active",
+            "source": "user",
+            "is_combo_product": false,
+            "is_linked_with_zohocrm": false,
+            "zcrm_product_id": "",
+            "description": "",
+            "brand": "",
+            "manufacturer": "",
+            "rate": 350.00,
+            "tax_id": "",
+            "tax_name": "",
+            "tax_percentage": 0,
+            "purchase_account_id": "3850050000000034003",
+            "purchase_account_name": "Cost of Goods Sold",
+            "account_id": "3850050000000000388",
+            "account_name": "Sales",
+            "purchase_description": "",
+            "purchase_rate": 200.00,
+            "item_type": "inventory",
+            "product_type": "goods",
+            "stock_on_hand": 0.000000,
+            "has_attachment": false,
+            "is_returnable": true,
+            "available_stock": 0.000000,
+            "actual_available_stock": 0.000000,
+            "sku": "D345",
+            "upc": "",
+            "ean": "",
+            "isbn": "",
+            "part_number": "",
+            "reorder_level": "",
+            "image_name": "",
+            "image_type": "",
+            "image_document_id": "",
+            "created_time": "2024-02-03T19:00:04+0200",
+            "last_modified_time": "2024-02-24T17:23:44+0200",
+            "length": "",
+            "width": "",
+            "height": "",
+            "weight": "",
+            "weight_unit": "kg",
+            "dimension_unit": "cm"
+        },
+        {
+            "item_id": "3850050000000083099",
+            "name": "Dummy Item",
+            "item_name": "Dummy Item",
+            "unit": "sku2",
+            "status": "active",
+            "source": "user",
+            "is_combo_product": false,
+            "is_linked_with_zohocrm": false,
+            "zcrm_product_id": "",
+            "description": "",
+            "brand": "",
+            "manufacturer": "",
+            "rate": 100.00,
+            "tax_id": "",
+            "tax_name": "",
+            "tax_percentage": 0,
+            "purchase_account_id": "3850050000000034003",
+            "purchase_account_name": "Cost of Goods Sold",
+            "account_id": "3850050000000000388",
+            "account_name": "Sales",
+            "purchase_description": "",
+            "purchase_rate": 50.00,
+            "item_type": "inventory",
+            "product_type": "goods",
+            "stock_on_hand": 14.000000,
+            "has_attachment": false,
+            "is_returnable": true,
+            "available_stock": 14.000000,
+            "actual_available_stock": 14.000000,
+            "sku": "sku1",
+            "upc": "",
+            "ean": "",
+            "isbn": "",
+            "part_number": "",
+            "reorder_level": 1.000000,
+            "image_name": "",
+            "image_type": "",
+            "image_document_id": "",
+            "created_time": "2023-05-10T22:10:27+0200",
+            "last_modified_time": "2023-05-10T23:13:25+0200",
+            "length": "",
+            "width": "",
+            "height": "",
+            "weight": "",
+            "weight_unit": "kg",
+            "dimension_unit": "cm"
+        },
+        {
+            "item_id": "3850050000000077005",
+            "name": "OIL FILTER GUD OD76 H79 [3/4 - 16UNF]",
+            "item_name": "OIL FILTER GUD OD76 H79 [3/4 - 16UNF]",
+            "unit": "Z587",
+            "status": "active",
+            "source": "user",
+            "is_combo_product": false,
+            "is_linked_with_zohocrm": false,
+            "zcrm_product_id": "",
+            "description": "",
+            "brand": "",
+            "manufacturer": "",
+            "rate": 85.00,
+            "tax_id": "",
+            "tax_name": "",
+            "tax_percentage": 0,
+            "purchase_account_id": "3850050000000034003",
+            "purchase_account_name": "Cost of Goods Sold",
+            "account_id": "3850050000000000388",
+            "account_name": "Sales",
+            "purchase_description": "",
+            "purchase_rate": 60.00,
+            "item_type": "inventory",
+            "product_type": "goods",
+            "stock_on_hand": 1.000000,
+            "has_attachment": true,
+            "is_returnable": true,
+            "available_stock": 1.000000,
+            "actual_available_stock": 1.000000,
+            "sku": "Z587",
+            "upc": "",
+            "ean": "",
+            "isbn": "",
+            "part_number": "",
+            "reorder_level": 1.000000,
+            "image_name": "Z587-profile.jpg",
+            "image_type": "jpg",
+            "image_document_id": "3850050000000077012",
+            "created_time": "2023-02-07T11:44:42+0200",
+            "last_modified_time": "2024-02-26T15:44:57+0200",
+            "length": "",
+            "width": "",
+            "height": "",
+            "weight": "",
+            "weight_unit": "kg",
+            "dimension_unit": "cm"
+        },
+        {
+            "item_id": "3850050000000101013",
+            "name": "Online Request Service Fee",
+            "item_name": "Online Request Service Fee",
+            "unit": "OSF",
+            "status": "active",
+            "source": "user",
+            "is_combo_product": false,
+            "is_linked_with_zohocrm": false,
+            "zcrm_product_id": "",
+            "description": "",
+            "brand": "",
+            "manufacturer": "",
+            "rate": 1.00,
+            "tax_id": "",
+            "tax_name": "",
+            "tax_percentage": 0,
+            "purchase_account_id": "3850050000000034003",
+            "purchase_account_name": "Cost of Goods Sold",
+            "account_id": "3850050000000000388",
+            "account_name": "Sales",
+            "purchase_description": "",
+            "purchase_rate": 0.00,
+            "item_type": "sales_and_purchases",
+            "product_type": "service",
+            "has_attachment": false,
+            "is_returnable": false,
+            "sku": "OSF",
+            "upc": "",
+            "ean": "",
+            "isbn": "",
+            "part_number": "",
+            "image_name": "",
+            "image_type": "",
+            "image_document_id": "",
+            "created_time": "2024-01-23T20:00:17+0200",
+            "last_modified_time": "2024-01-23T20:00:17+0200",
+            "length": "",
+            "width": "",
+            "height": "",
+            "weight": "",
+            "weight_unit": "kg",
+            "dimension_unit": "cm"
+        }
     ],
-    page_context: {}
-}
+    "page_context": {
+        "page": 1,
+        "per_page": 200,
+        "has_more_page": false,
+        "report_name": "Items",
+        "applied_filter": "Status.All",
+        "custom_fields": [],
+        "sort_column": "name",
+        "sort_order": "A"
+    }
+  }
+
+  type PageContext = {
+    page: number;
+    per_page: number;
+    has_more_page: boolean;
+    report_name: string;
+    applied_filter: string;
+    custom_fields: any[];
+    sort_column: string;
+    sort_order: string;
+  }
 
 
 type BaseResponse<T> = {
     code: number;
     message: string;
-    data: T;
-    page_context: unknown;
+    data?: T;
+    page_context?: PageContext;
 }
 type RequestParams = {
     organization_id: string;
@@ -90,7 +300,6 @@ type GenericQueryParams<T> = {
 
 
 const access_token = 'my-access-token'
-
 
 
 
@@ -137,15 +346,29 @@ const obj = {...obj_1, ...obj_2}
 
 }
 
-const zoho = (request: RequestParams) => {
+
+const getResourceKey = (obj: any): string => {
+const objectKeys = Object.keys(obj)
+const resourceKey = objectKeys.find(i => i !== 'code' && i!== 'page_context' && i !== 'message') ||  ''
+return resourceKey
+
+}
+const zoho = async (request: RequestParams): Promise<BaseResponse<unknown>> => {
 
 const resource_searchParams = convertObjToUrl(request.query_params.resource_params, request.query_params.custom_params)
 
 const baseUrl = `https://www.zohoapis.com/inventory/v1/${request.resource}?organization_id=${request.organization_id}&${resource_searchParams}`
 
-// console.log(baseUrl)
-console.log('requesttype', request.request_type)
+const response = dummy_items
+const resourceKey = getResourceKey(response)
 
+const modifiedResponse = {...response, data: response['items']}
+
+
+// console.log(baseUrl)
+// console.log('requesttype', request.request_type)
+
+return dummy_items
 
 }
  
